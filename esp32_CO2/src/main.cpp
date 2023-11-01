b6e18c4de2ea4d58da01c4c6985b82f2bb0f2893
apicov <a.pico@posteo.de>
2023-11-01 23:01:54 +0100

    add wifi connection check (and led that toggles while connecting)

--- esp32_CO2/src/main.cpp
+++ esp32_CO2/src/main.cpp
@@ -8,6 +8,8 @@
 #include "RTClib.h"
 #include "private_data.h"
 
+int LED_BUILTIN = 2;
+
 RTC_DS3231 rtc;
 
 
@@ -33,8 +35,11 @@ void initWiFi() {
   WiFi.begin(SSID, PASSWORD);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, HIGH);
     Serial.print('.');
-    delay(1000);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
   }
   Serial.println(WiFi.localIP());
 }
@@ -129,7 +134,9 @@ int send_scd30_to_server(SCD30_DATA data, char * timestamp)
 
 
 void setup() {
+  pinMode (LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  
 
   initWiFi();
   Serial.print("RRSI: ");
@@ -153,7 +160,7 @@ void setup() {
   Serial.println("SCD30 Found!");
   delay(100);
 
-   if (!scd30.setMeasurementInterval(10)){
+   if (!scd30.setMeasurementInterval(30)){
      Serial.println("Failed to set measurement interval");
      while(1){ delay(10);}
    }
@@ -168,8 +175,13 @@ void loop() {
   Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
   Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
   delay(13000);*/
+  if (WiFi.status() != WL_CONNECTED){
+    initWiFi();
+    Serial.print("wifi connected.");
+  }
 
   int resp = get_scd30_data(&scd30_data);
+
   DateTime timestamp = rtc.now();
   char str_datetime[20];
   sprintf(str_datetime, "%04d-%02d-%02d %02d:%02d:%02d", timestamp.year(),
@@ -192,89 +204,3 @@ void loop() {
   delay(10000);
 }
 
-
-
-
-void WiFiEvent(WiFiEvent_t event) {
-  Serial.printf("[WiFi-event] event: %d\n", event);
-
-  switch (event) {
-    case SYSTEM_EVENT_WIFI_READY: 
-      Serial.println("WiFi interface ready");
-      break;
-    case SYSTEM_EVENT_SCAN_DONE:
-      Serial.println("Completed scan for access points");
-      break;
-    case SYSTEM_EVENT_STA_START:
-      Serial.println("WiFi client started");
-      break;
-    case SYSTEM_EVENT_STA_STOP:
-      Serial.println("WiFi clients stopped");
-      break;
-    case SYSTEM_EVENT_STA_CONNECTED:
-      Serial.println("Connected to access point");
-      break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-      Serial.println("Disconnected from WiFi access point");
-      WiFi.begin(SSID, PASSWORD);
-      break;
-    case SYSTEM_EVENT_STA_AUTHMODE_CHANGE:
-      Serial.println("Authentication mode of access point has changed");
-      break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-      Serial.print("Obtained IP address: ");
-      Serial.println(WiFi.localIP());
-      break;
-    case SYSTEM_EVENT_STA_LOST_IP:
-      Serial.println("Lost IP address and IP address is reset to 0");
-      break;
-    case SYSTEM_EVENT_STA_WPS_ER_SUCCESS:
-      Serial.println("WiFi Protected Setup (WPS): succeeded in enrollee mode");
-      break;
-    case SYSTEM_EVENT_STA_WPS_ER_FAILED:
-      Serial.println("WiFi Protected Setup (WPS): failed in enrollee mode");
-      break;
-    case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:
-      Serial.println("WiFi Protected Setup (WPS): timeout in enrollee mode");
-      break;
-    case SYSTEM_EVENT_STA_WPS_ER_PIN:
-      Serial.println("WiFi Protected Setup (WPS): pin code in enrollee mode");
-      break;
-    case SYSTEM_EVENT_AP_START:
-      Serial.println("WiFi access point started");
-      break;
-    case SYSTEM_EVENT_AP_STOP:
-      Serial.println("WiFi access point  stopped");
-      break;
-    case SYSTEM_EVENT_AP_STACONNECTED:
-      Serial.println("Client connected");
-      break;
-    case SYSTEM_EVENT_AP_STADISCONNECTED:
-      Serial.println("Client disconnected");
-      break;
-    case SYSTEM_EVENT_AP_STAIPASSIGNED:
-      Serial.println("Assigned IP address to client");
-      break;
-    case SYSTEM_EVENT_AP_PROBEREQRECVED:
-      Serial.println("Received probe request");
-      break;
-    case SYSTEM_EVENT_GOT_IP6:
-      Serial.println("IPv6 is preferred");
-      break;
-    case SYSTEM_EVENT_ETH_START:
-      Serial.println("Ethernet started");
-      break;
-    case SYSTEM_EVENT_ETH_STOP:
-      Serial.println("Ethernet stopped");
-      break;
-    case SYSTEM_EVENT_ETH_CONNECTED:
-      Serial.println("Ethernet connected");
-      break;
-    case SYSTEM_EVENT_ETH_DISCONNECTED:
-      Serial.println("Ethernet disconnected");
-      break;
-    case SYSTEM_EVENT_ETH_GOT_IP:
-      Serial.println("Obtained IP address");
-      break;
-    default: break;
-}}
